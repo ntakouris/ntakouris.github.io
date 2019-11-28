d31d890187bcb6b52b9297a4fed15454e4cae9cb
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-11-28 15:59:10 +0200

    Add project list

--- site/src/pages/Index.vue
+++ site/src/pages/Index.vue
@@ -46,79 +46,21 @@
     <h1>Definitely check these out while you hang around this website</h1>
     <br />
 
-    <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
+    <ProjectList />
 
-    <v-card class="mx-auto mt-4" max-width="344" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">Project</div>
-          <v-list-item-title class="headline mb-1">Algotrading Experiments</v-list-item-title>
-          <v-list-item-subtitle>Some Deep Learning Experiment I am doing with <a href="https://github.com/deepettas" target="_blank">@deepettas</a> et al. <br />
-          </v-list-item-subtitle>
-          <div class="text--primary"><br />
-            Basically a 3-part project: Data Acquisition, a functional Rest API and the ML experiments.
-          </div>
-        </v-list-item-content>
-
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn text href="https://github.com/bitaesthetics/algotrading-api" target="_blank">Source</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card class="mx-auto mt-4" max-width="344" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">Tutorial</div>
-          <v-list-item-title class="headline mb-1">.Net Core Rest API</v-list-item-title>
-          <v-list-item-subtitle>Videos and articles in collab with <a href="https://github.com/Elfocrash" target="_blank">@elfocrash</a><br />
-          </v-list-item-subtitle>
-
-          <div class="text--primary"><br />
-            A big spectrum of concepts is covered. Difficulty is beginner/intermediate. View the videos or read the instructional articles.
-          </div>
-        </v-list-item-content>
-
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn text href="https://github.com/Elfocrash/Youtube.AspNetCoreTutorial" target="_blank">Source</v-btn>
-        <v-btn text href="https://medium.com/@zarkopafilis/asp-net-core-2-2-3-rest-api-tutorials-index-e322afd39348?source=friends_link&sk=5c95258dcb116067588c1d9c0eb8b297" target="_blank">Medium</v-btn>
-        <v-btn text href="https://www.youtube.com/watch?v=sdlt3-ptt9g&list=PLUOequmGnXxOgmSDWU7Tl6iQTsOtyjtwU" target="_blank">Youtube</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card class="mx-auto mt-4" max-width="344" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">Project</div>
-          <v-list-item-title class="headline mb-1">Go SQL Server</v-list-item-title>
-          <v-list-item-subtitle>Query tabular and not only data. Powered by Go.<br />
-          </v-list-item-subtitle>
-
-          <div class="text--primary"><br />
-            Interesting project. I did contributions on the implementations of several SQL commands and tests, as well as bug reports, during hacktoberfest.
-          </div>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn text href="https://github.com/src-d/go-mysql-server" target="_blank">Source</v-btn>
-      </v-card-actions>
-    </v-card>
-    </v-layout>
-    </v-container>
-
-  <br /><br />
+    <br /><br />
   </Layout>
 </template>
 
 <script>
+import ProjectList from '~/components/ProjectList.vue'
+
 export default {
   metaInfo: {
     title: 'Home'
+  },
+  components: {
+    ProjectList
   }
 }
 </script>
---
+++ site/src/pages/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <ProjectList />
+  </Layout>
+</template>
+
+<script>
+import ProjectList from '~/components/ProjectList.vue'
+
+export default {
+  name: "Projects",
+  components: {
+    ProjectList
+  }
+};
+</script>
+
+<style>
+</style>
